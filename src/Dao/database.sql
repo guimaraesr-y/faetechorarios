26deaf17c741672852fad0dd1054c4db36a53ff8
Ryan Guimarães <guimaraesryan26@gmail.com>
2023-08-29 13:54:40 -0300

    [refactor] sql para criação do banco refatorado

--- src/Dao/database.sql
+++ src/Dao/database.sql
@@ -2,19 +2,9 @@
 -- Servidor:                     127.0.0.1
 -- Versão do servidor:           8.0.31 - MySQL Community Server - GPL
 -- OS do Servidor:               Win64
--- HeidiSQL Versão:              11.2.0.6213
 -- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Copiando estrutura do banco de dados para faetechorarios
-CREATE DATABASE IF NOT EXISTS `faetechorarios` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `faetechorarios`
 USE `faetechorarios`;
 
 -- Copiando estrutura para tabela faetechorarios.horarios
@@ -29,9 +19,7 @@ CREATE TABLE IF NOT EXISTS `horarios` (
   KEY `horarios_ibfk_2` (`professor_turma_id`),
   CONSTRAINT `horarios_ibfk_1` FOREIGN KEY (`sala_id`) REFERENCES `salas` (`id`) ON DELETE CASCADE,
   CONSTRAINT `horarios_ibfk_2` FOREIGN KEY (`professor_turma_id`) REFERENCES `professor_turma` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Exportação de dados foi desmarcado.
+);
 
 -- Copiando estrutura para tabela faetechorarios.professores
 CREATE TABLE IF NOT EXISTS `professores` (
@@ -39,9 +27,7 @@ CREATE TABLE IF NOT EXISTS `professores` (
   `nome` varchar(255) NOT NULL,
   `tempos_semanais` int NOT NULL DEFAULT '20',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Exportação de dados foi desmarcado.
+);
 
 -- Copiando estrutura para tabela faetechorarios.professor_turma
 CREATE TABLE IF NOT EXISTS `professor_turma` (
@@ -53,18 +39,14 @@ CREATE TABLE IF NOT EXISTS `professor_turma` (
   KEY `professor_turma_ibfk_2` (`turma_id`),
   CONSTRAINT `professor_turma_ibfk_1` FOREIGN KEY (`professor_id`) REFERENCES `professores` (`id`) ON DELETE CASCADE,
   CONSTRAINT `professor_turma_ibfk_2` FOREIGN KEY (`turma_id`) REFERENCES `turmas` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Exportação de dados foi desmarcado.
+);
 
 -- Copiando estrutura para tabela faetechorarios.salas
 CREATE TABLE IF NOT EXISTS `salas` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Exportação de dados foi desmarcado.
+);
 
 -- Copiando estrutura para tabela faetechorarios.turmas
 CREATE TABLE IF NOT EXISTS `turmas` (
@@ -72,11 +54,4 @@ CREATE TABLE IF NOT EXISTS `turmas` (
   `nome` varchar(255) NOT NULL,
   `turno` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Exportação de dados foi desmarcado.
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+);
